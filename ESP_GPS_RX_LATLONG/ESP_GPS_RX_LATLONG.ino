83e8cb7dbd36f3a22e7585e3c9451fe3dac4b113
Luke <lhmelikian@gmail.com>
2026-04-28 19:13:44 -0700

    added twai transmission and fixed the bugs with it. should be ready to test as-is right now

--- ESP_GPS_RX_LATLONG/ESP_GPS_RX_LATLONG.ino
+++ ESP_GPS_RX_LATLONG/ESP_GPS_RX_LATLONG.ino
@@ -2,6 +2,13 @@
 #include "I2C_Custom.h"
 #include "nmea_translate.h"
 
+// canbus twai
+#include "driver/twai.h"
+
+// can pins
+#define CAN_TX_PIN 16
+#define CAN_RX_PIN 17
+
 // neo m8p slave address
 #define SLAVE_ADDR 0x42
 
@@ -22,9 +29,45 @@ uint16_t availableBytes = 0;
 Location myLocation;
 
 I2C_Custom i2c;
+
+// message object for twai
+
+twai_message_t message;
+
+/* this will send a double */
+void twai_sendDouble(twai_message_t& msg, uint16_t id, double value) {
+  msg.identifier = id;
+  memcpy(message.data, &value, sizeof(value));
+  twai_transmit(&msg, pdMS_TO_TICKS(1000));
+}
+
 void setup() {
   i2c.begin();
   serial_begin(115200);
+
+  // some config
+  twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT((gpio_num_t) CAN_TX_PIN, (gpio_num_t) CAN_RX_PIN, TWAI_MODE_NORMAL);
+  twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+  twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+  if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
+    Serial.println("Driver installed");
+  } else {
+    Serial.println("Failed to install driver");
+    return;
+  }
+
+  if (twai_start() == ESP_OK) {
+    Serial.println("Driver started");
+  } else {
+    Serial.println("Failed to start driver");
+    return;
+  }
+
+  message.extd = 0;  // 0 for 11-bit ID, 1 for 29-bit
+  message.rtr = 0;   // not a remote transmission request
+  message.ss = 0;    // not single-shot (allow retries)
+  message.self = 0;  // not a self-reception
 }
 
 void loop() {
@@ -36,10 +79,10 @@ void loop() {
   GPS_LSB = i2c.readI2CReg(SLAVE_ADDR, DATA_SIZE_LSB_ADDR);
 
   // get our total number of bytes
-  availableBytes = static_cast<uint16_t> (GPS_MSB) << 8 | static_cast<uint16_t> (GPS_LSB);
+  availableBytes = static_cast<uint16_t>(GPS_MSB) << 8 | static_cast<uint16_t>(GPS_LSB);
   serial_print("Available Bytes: ");
   serial_println(availableBytes);
-  
+
   // once we know how much is available, read it into our data buff
   if (availableBytes > 0) {
     for (int i = 0; i < availableBytes; i++) {
@@ -49,11 +92,19 @@ void loop() {
 
     myLocation = translateGNRMC(nmeaData);
 
+
+    message.data_length_code = 8;
+
+    // send latitude
+    twai_sendDouble(message, 0x123, myLocation.latitude);
+
+    twai_sendDouble(message, 0x124, myLocation.longitude);
+
     // finally, let us print what we get
     serial_println(myLocation.latitude);
     serial_println(myLocation.longitude);
   }
-  
+
   // delay for neo m8p to fill internal buffers again with data
   delay(200);
 }
